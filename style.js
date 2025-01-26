1f7809803ec253de2e2a06f2b34311071507251a
なかっと <123220287+nakatto-k@users.noreply.github.com>
2025-01-26 11:30:31 +0900

    Update style.js

--- style.js
+++ style.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("ページが読み込まれました！");
 });
-const textBox = document.getElementById("試験時間");
-const inputValue = textBox.value;
-console.log("入力された値:", inputValue);
+document.getElementById('試験時間')
+.addEventListener('click', () => { ... })
+const inputValue = document.getElementById('textInput').value;
