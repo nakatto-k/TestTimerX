3d331cfbadbbfb4c397fadb3a853f9196f6e9650
なかっと <123220287+nakatto-k@users.noreply.github.com>
2025-01-31 15:27:28 +0900

    Add files via upload

---
+++ 0/style.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("ページが読み込まれました！");
+});
+document.getElementById('試験時間')
+.addEventListener('click', () => { ... })
+const 試験時間 = document.getElementById('試験時間').value;
+console.log('入力された値:', 試験時間);
+window.location.href = 'https://www.nakatto-k.github.io/TestTimerX/setuplistening';
+});
